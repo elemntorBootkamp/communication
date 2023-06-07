3c64cc4b99d234c43e3a0433ec0d375d385f2778
tamargit1 <133575298+tamargit1@users.noreply.github.com>
2023-06-07 14:37:27 +0300

    update

--- logger.js
+++ logger.js
@@ -1,56 +1,4 @@
 
-// const winston = require('winston');
-// const chalk = require('chalk');
-// //אוביקט לוגר
-// const logger = winston.createLogger({
-//   level: 'info',
-//   format: winston.format.combine(
-//     winston.format.timestamp(),
-//     winston.format.json()
-//   ),
-//   //תעבורה לקונסול
-//   transports: [
-//     new winston.transports.Console({
-//       format: winston.format.combine(
-//         winston.format.colorize(),
-//         winston.format.printf(info => {
-//           const { level, message, timestamp } = info;
-//           const coloredMessage = level === 'warn' ? chalk.yellow(message) : message;
-//           return `${timestamp} [${level}]: ${coloredMessage}`;
-//         })
-//       )
-//     }),
-//     //תעבורה לקובץ
-//     new winston.transports.File({
-//       filename: 'logfile.log',
-//       format: winston.format.combine(
-//         winston.format.printf(info => {
-//           const { level, message, timestamp } = info;
-//           const coloredMessage = level === 'warn' ? chalk.yellow(message) : message;
-//           return `${timestamp} [${level}]: ${coloredMessage}`;
-//         })
-//       )
-//     })
-//   ]
-// });
-// // logger.info('Starting the application...');
-// const users = [
-//   { id: 1, name: 'John Doe' },
-//   { id: 2, name: 'Jane Doe' },
-//   { id: 3, name: 'Bob Smith' }
-// ];
-// // if(users.length==3){
-// // logger.info(`Found ${users.length} users`);
-// // logger.warn('This is a warning message');
-// // logger.warn(chalk.yellow('This is another warning message'));
-// // module.exports = logger;
-// // }
-
-// try {1000/0
-//   // your1000 code that may throw an error
-// } catch (error) {
-//   logger.error(error);
-// }
 
 const winston = require('winston');
 const chalk = require('chalk');
@@ -86,18 +34,18 @@ const logger = winston.createLogger({
     })
   ]
 });
-// logger.info('Starting the application...');
-// const users = [
-//   { id: 1, name: 'John Doe' },
-//   { id: 2, name: 'Jane Doe' },
-//   { id: 3, name: 'Bob Smith' }
-// ];
-// if(users.length==3){
-// logger.info(`Found ${users.length} users`);
-// logger.warn('This is a warning message');
-// logger.warn(chalk.yellow('This is another warning message'));
-// module.exports = logger;
-// }
+logger.info('Starting the application...');
+const users = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Doe' },
+  { id: 3, name: 'Bob Smith' }
+];
+if(users.length==3){
+logger.info(`Found ${users.length} users`);
+logger.warn('This is a warning message');
+logger.warn(chalk.yellow('This is another warning message'));
+module.exports = logger;
+}
 
 try {
   1000/0
